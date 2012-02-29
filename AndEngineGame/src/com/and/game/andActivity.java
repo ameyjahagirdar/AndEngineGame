7f1116f40dcc347536dbf31239c7bfa6017d188f
Hossam Gharib <eng.hossam.semsem@gmail.com>
2012-02-29 03:07:20 +0200

    Adding of Splash Screen

--- AndEngineGame/src/com/and/game/andActivity.java
+++ AndEngineGame/src/com/and/game/andActivity.java
@@ -1,14 +1,52 @@
 package com.and.game;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
 
-public class andActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.out.println("hossam");
-        setContentView(R.layout.main);
-    }
+import android.view.Display;
+
+
+public class andActivity extends BaseGameActivity  {
+	//private
+	private Camera mCamera;
+	private Scene mainScene;
+
+	@Override
+	public Engine onLoadEngine() {
+		final Display display = getWindowManager().getDefaultDisplay();
+		mCamera = new Camera(0, 0, display.getWidth(), display.getHeight());
+		
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+				         new RatioResolutionPolicy(mCamera.getWidth(), mCamera.getHeight()),
+				         mCamera));
+	}
+
+	@Override
+	public void onLoadResources() {
+		
+		
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		mEngine.registerUpdateHandler(new FPSLogger());
+		mainScene = new Scene();
+		mainScene.setBackground(new ColorBackground(0.09f, 0.6f, 0.8f));
+		return mainScene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+		
+		
+	}
+    
+  
 }
